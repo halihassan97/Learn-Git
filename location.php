1c0b6459fd8c3e650d3a0fce437a7a2a03517378
Fahad <halihassan97@gmail.com>
2022-06-27 01:19:21 +0500

    adding features

--- location.php
+++ location.php
@@ -5,7 +5,7 @@ $addr_details = @unserialize(file_get_contents('http://www.geoplugin.net/php.gp?
 $city = $addr_details['geoplugin_city'];
 $countrycode = $addr_details['geoplugin_countryCode'];
 $country = $addr_details['geoplugin_countryName'];
- echo "<b>"."City Name:"."</b>".$city."<br>";
+ echo "<b>"."You City Name:"."</b>".$city."<br>";
 echo "<b>"."Country Code:"."</b>".$countrycode."<br>";
 echo "<b>"."Country Name:"."</b>".$country."<br>";
 ?>
